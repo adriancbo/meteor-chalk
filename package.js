bb1dc82e09ee18d4c9cc5f51ce77fbf02a266423
Adrian Carballo <netvineco@gmail.com>
2015-06-12 14:59:33 -0400

    update description

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'adriancbo:chalk',
   version: '1.0.0',
-  summary: 'Tiny Meteor wrapper for chalk',
+  summary: 'A Tiny Meteor wrapper around the quintessential Chalk.',
   git: 'https://github.com/adriancbo/meteor-chalk.git',
   documentation: 'README.md'
 });
